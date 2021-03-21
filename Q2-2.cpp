c66ce01735453736542f8b8bee061c6519c2341a
KarishaDel <karsishadelrosario@gmail.com>
2021-03-21 09:16:27 -0700

    10min

---
+++ Q2-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int randum;
+    int min = 100, max=0;
+
+    srand(time(0));
+    for (int i; i < 5; i++)
+    {
+       randum = rand() % 100;  
+       cout << randum << endl;
+
+       if(randum > max);
+        max = randum;
+       
+       if(randum > ) 
+
+    }
+     
+}
