5a5e81982b72f4b4ac7ee8157ba2d0868a2d687e
KarishaDel <karsishadelrosario@gmail.com>
2021-03-21 11:23:52 -0700

    eh

--- Q2-2.cpp
+++ Q2-2.cpp
@@ -8,21 +8,28 @@ int main()
 {
     int randum;
     int min = 100, max=0;
+    int num;
 
     srand(time(0));
     for (int i; i < 5; i++)
     {
        randum = rand() % 100;  
-       cout << randum << endl;      
-    if(randum>max) 
-        max=randum;
-    if(randum<min) 
-       min=randum;        
+       cout << randum << endl;               
     }
 
+    do
+    {
+       if(randum>max) 
+        max=randum;
+       if(randum<min) 
+        min=randum;
+    } while (/* condition */);
+    
 
 
 
+cout << max;
+cout << min;
    
     
      
