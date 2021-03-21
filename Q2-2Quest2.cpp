3206669f1cb18e2a87719f02bd3305b86f3c98c8
KarishaDel <karsishadelrosario@gmail.com>
2021-03-21 11:01:45 -0700

    dang

--- Q2-2Quest2.cpp
+++ Q2-2Quest2.cpp
@@ -4,10 +4,17 @@ using namespace std;
 int main()
 {
     int number1, number2, number3;
+    const int min{0};
+    const int max {99};
 
      cout << "Enter three integer values from 0 to 99: " ;
      cin >> number1 >> number2 >> number3 ;
     
+    if (number1, number2 ,number3 < min  || number1,number2,number3 > max)
+    cout<< "Try again"<< endl;
+
+    else
+    {
         if (number1 != number2 && number1 != number3 && number2 != number3)
             cout << "No equal numbers."; 
 
@@ -19,7 +26,7 @@ int main()
 
         else if(number1 == number3)
          cout << number1 << " = " << number3 << endl;
- 
+    }
 
      return 0;
 }
