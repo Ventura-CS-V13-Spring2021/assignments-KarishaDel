ae035f872b545af5bbdf031bd289c986e93fbba6
KarishaDel <karsishadelrosario@gmail.com>
2021-03-21 14:12:58 -0700

    ok

--- Q8-2.cpp
+++ Q8-2.cpp
@@ -9,19 +9,24 @@ int main ()
     string emNM;
     string emDP;
     double salary;
+    double sum = 0.0;
+    int count = 0; 
+    float avg;
 
     ifs.open("EmployeeRecord.txt");
 
 while(ifs >> empID >> emNM >> emDP >> salary)
 {
+    count++; 
+    sum += salary;
     cout << "ID: " << empID<< endl;
     cout << "Name: " << emNM << endl;
     cout << "Department: " << emDP << endl;
-    cout << "Salary: " << salary << endl; 
+    cout << "Salary: " << salary << endl;
+
+}          
+    cout << "Total: " << sum << endl;
+    cout << count << endl;
+    avg = sum/count;
+    cout << "Avarage: " << avg << endl;
 }
-
-        
-
-}
-
-
