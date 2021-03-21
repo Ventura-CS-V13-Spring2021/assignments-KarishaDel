c7ab5228e9743c1eeb134fba88f71f11a2ac03c1
KarishaDel <karsishadelrosario@gmail.com>
2021-03-21 11:38:20 -0700

    eh

---
+++ Q8.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	ofstream 	ERfile;
+    int emamt;
+    int empID;
+    string emNM;
+    string emDP;
+    double salary;
+
+	ERfile.open("EmployeeRecord.txt");
+
+    cout << "Hoaw many";
+    cin >> emamt;
+
+
+
+	ERfile.close();
+}    
