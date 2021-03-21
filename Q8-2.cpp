cd1165a4669ddaaed58a8e2db57d74dac9b1fb7c
KarishaDel <karsishadelrosario@gmail.com>
2021-03-21 13:52:30 -0700

    eh

--- Q8-2.cpp
+++ Q8-2.cpp
@@ -9,26 +9,19 @@ int main ()
     string emNM;
     string emDP;
     double salary;
-    int emamt;
+
     ifs.open("EmployeeRecord.txt");
 
-    cout << "How many files would you like to compare?" << endl;
-    cin >> emamt;
-    
-    for(int i; i < emamt; i++)
-    {
-        ifs >> empID;
-        ifs >> emNM;
-        ifs >> emDP;
-        ifs >> salary;
-        cout << "ID: " << empID<< endl;
-        cout << "Name: " << emNM << endl;
-        cout << "Department: " << emDP << endl;
-        cout << "Salary: " << salary << endl; 
-    }
-    
- 
+while(ifs >> empID >> emNM >> emDP >> salary)
+{
+    cout << "ID: " << empID<< endl;
+    cout << "Name: " << emNM << endl;
+    cout << "Department: " << emDP << endl;
+    cout << "Salary: " << salary << endl; 
+}
+
         
 
 }
+
 
