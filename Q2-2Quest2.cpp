787ffe470b9fd3c5b51ca35d16d94f4a3827a107
KarishaDel <karsishadelrosario@gmail.com>
2021-03-21 10:20:49 -0700

    eh

--- Q2-2Quest2.cpp
+++ Q2-2Quest2.cpp
@@ -5,27 +5,21 @@ int main()
 {
     int number1, number2, number3;
 
-     cout << "Enter three integer values : " ;
+     cout << "Enter three integer values from 0 to 99: " ;
      cin >> number1 >> number2 >> number3 ;
+    
+        if (number1 != number2 && number1 != number3 && number2 != number3)
+            cout << "No equal numbers."; 
 
-        if(number1 == number2)
-        {
-            cout << number1 << " and " << number2;
-        } 
+        else if(number1 == number2)
+         cout << number1 << " = " << number2 << endl;
 
-        if(number2 == number3)
-        {
-            cout << number2 << " and " << number3;
-        } 
+        else if(number2 == number3)
+         cout << number2 << " = " << number3 << endl; 
 
-        if(number3 == number1)
-        {
-            cout << number3 << " and " << number1;
-        }
+        else if(number1 == number3)
+         cout << number1 << " = " << number3 << endl;
+ 
 
-        
-        {
-            cout << "None are equal.";
-        }
      return 0;
 }
