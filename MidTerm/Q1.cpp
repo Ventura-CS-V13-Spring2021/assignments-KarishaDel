aff60d392ce483e2a5689d233e6ed2e3ffbb5c88
KarishaDel <karsishadelrosario@gmail.com>
2021-04-15 15:30:17 -0700

    3

--- MidTerm/Q1.cpp
+++ MidTerm/Q1.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 
 using namespace std;
-void getInput(char, int &, int &);
-
+//void getInput(char, int &, int &);
+void fileWrt (ofstream &, string, int);
 int main()
 {
     ofstream StdntFile;
@@ -16,4 +16,20 @@ int main()
 
     cout << "How many student?" << endl;
     cin >> NumStd;
-}    
+
+    for (int i; i < NumStd; i++)
+    {
+        cout << "Name: "<< endl;
+        cin >> StdNm;
+        StdntFile << StdNm<< endl;
+        cout << "Grade 1: " << endl;
+        cin >> Grd1;
+        StdntFile << Grd1<< endl;
+        cout << "Grade 2: " << endl;
+        StdntFile << Grd2<< endl;
+    }    
+}   
+void fileWrt (ofstream &StdntFile, int Grd1, int Grd2)
+{
+    
+} 
