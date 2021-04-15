277050fbbdac8993b8863ac9a3ac6622b15a3266
KarishaDel <karsishadelrosario@gmail.com>
2021-04-15 16:14:48 -0700

    6

---
+++ MidTerm/Q1Rd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main ()
+{
+    ifstream ifs;
+    string StdNm;
+    int Grd1;
+    int Grd2;
+    int NumStd;
+    double sum = 0.0;
+    int count = 0; 
+    float avg;
