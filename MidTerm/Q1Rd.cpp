98074999ad1fc3e799cf8d39d0ecaf278c15b82a
KarishaDel <karsishadelrosario@gmail.com>
2021-04-15 16:26:31 -0700

    8

--- MidTerm/Q1Rd.cpp
+++ MidTerm/Q1Rd.cpp
@@ -12,3 +12,19 @@ int main ()
     double sum = 0.0;
     int count = 0; 
     float avg;
+
+    ifs.open("Student.txt");
+
+while(ifs >> Grd1 >> Grd2 >> StdNm)
+{
+ 
+    sum = Grd1 + Grd2;
+    cout << "Name: " << StdNm<< endl;
+    cout << "Grade 1: " << Grd1 << endl;
+    cout << "Grade 2: " << Grd2 << endl;
+}
+    cout << "Total: " << sum << endl;
+    cout << count << endl;
+    avg = sum/10;
+    cout << "Avarage: " << avg << endl;
+}
