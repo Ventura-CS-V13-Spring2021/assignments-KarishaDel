fcf0ba0468f09a0709dfb5fd41872dc36e12bc01
KarishaDel <karsishadelrosario@gmail.com>
2021-04-15 17:48:58 -0700

    3

--- MidTerm/Q2.cpp
+++ MidTerm/Q2.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
+int isGreater(int);
+int getRnd ();
 
 int main()
 {
-
+    int N = 10;
+    int min = 1;
+    int max = 50;
 
     ofstream NFile;
     NFile.open("Numbers.txt");
@@ -14,9 +20,11 @@ int main()
         cerr << "Error.";
         exit(0);
     }
-
-
     NFile.close();
 
    
+} 
+void getRnd(int)
+{
+    return
 } 
