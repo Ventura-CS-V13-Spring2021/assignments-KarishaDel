5d16fd2f9361b7d186c4d2b6dcc56fe2ec7accc4
KarishaDel <karsishadelrosario@gmail.com>
2021-04-15 17:37:58 -0700

    1

--- MidTerm/Q2.cpp
+++ MidTerm/Q2.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
 
-}    
+
+    ofstream NFile;
+    NFile.open("Numbers.txt");
+    if (NFile.fail())
+    {
+        cerr << "Error.";
+        exit(0);
+    }
+
+
+    NFile.close();
+
+   
+} 
