b8dd5460991594439853e06c17b7a91570cb5f55
KarishaDel <karsishadelrosario@gmail.com>
2021-04-15 18:45:05 -0700

    6

--- MidTerm/Q2.cpp
+++ MidTerm/Q2.cpp
@@ -29,8 +29,6 @@ int main()
         NFile << num << endl;
     }
         
-
-
     NFile.close();
 } 
 
@@ -59,3 +57,6 @@ int isGreater(int num)
 
 }
 
+/* First I created the number generator using the libraries. I created two functions one to get the randum number then one to compare
+the numbers to its preciding number. A problem I ran into to that it was not giving me a steady increasing number line. I was getting
+36, 32, 16 then 37. I kept getting the first number included in the list. 
