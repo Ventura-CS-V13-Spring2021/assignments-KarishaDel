b081dca0f0a9ce8e8aec21cfce9dfaeba8b25207
KarishaDel <karsishadelrosario@gmail.com>
2021-05-08 20:50:34 -0700

    3

---
+++ Quiz/Quiz 7/rectangle.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "coordinate.hpp"
+#include "rectangle.hpp"
+using namespace std;
+
+Rectangle::Rectangle()
+{
+
+}
+Rectangle::Rectangle(Coordinate lbval, Coordinate rtval)
+{
+
+}
+
+Coordinate Rectangle::getLB() const
+{
+
+}
+
+Coordinate Rectangle::getRT() const
+{
+
+}
+
+double Rectangle::getArea()
+{
+
+}
+
+Coordinate Rectangle::getCenter()
+{
+
+}
+
+void Rectangle::setLBRT(Coordinate lbval, Coordinate rtval)
+{
+
+}
+
+void Rectangle::printRectangle() const
+{
+
+}
