7bc9554fba3d078abc3f275854e831dcb7ee3959
KarishaDel <karsishadelrosario@gmail.com>
2021-05-13 13:05:27 -0700

    4

--- Final/Q1.cpp
+++ Final/Q1.cpp
@@ -10,14 +10,17 @@ Find the element that has the greatest number of divisible elements(the number o
 using namespace std;
 
 int getNumDiv(int div, int a[], int N); //needed the N for the lenght of the array, div will be the devider
-void max(int N, int a[]);
-// void sortAcs(int a[], int length);
+
 
 int main()
 {
     const int N = 6;
-    int a[N] = {5,6,24,22,8,100};
-    int length = sizeof(a)/sizeof(a[0]); //get the length
+    int a[N] = {5,6,8,24,30,100};
+    // int length = sizeof(a)/sizeof(a[0]); //get the length
+    int max;
+    int num;
+    int div;
+
 
     cout << "Number List: \n"; //print the array
     for(int i = 0; i < N; i++)
@@ -25,8 +28,11 @@ int main()
         cout << a[i] << " ";
     }
 
-    max(N, a);
-    // sortAcs(a, length);
+    for (int i = 0; i < N; i++)
+    {
+        num = getNumDiv( div, a, N);
+    }
+
 
 }
 
@@ -42,20 +48,3 @@ int getNumDiv(int div, int a[], int N)
     return i;
 }
 
-void max(int N, int a[])
-{
-    int num;
-    num = a[N];
-
-    for (int i = 0; i < N; i++)
-    {
-        if (num < a[i])
-        num = a[i];
-    }
-    cout << "Max in list: " << num << endl;
-}
-
-// void sortAsc(int a[] , int length )
-// {
-//     sort(a , a + length);
-// }
