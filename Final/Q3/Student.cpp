068f5e674c3a055604c75e25fb2f3e2a9efee728
KarishaDel <karsishadelrosario@gmail.com>
2021-05-13 14:59:31 -0700

    1

---
+++ Final/Q3/Student.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+#include "Student.hpp"
+
+using namespace std;
+
+Student::~Student()
+{
+    resetClass();
+}
+
+Student& Student::operator=(const Student& rhs) 
+{
+    resetClass();
+
+    numClasses = rhs.numClasses;    
+    if(numClasses > 0)
+    {
+        classList = new string[numClasses];
+        for(int i = 0; i < numClasses; i++)
+        classList[i] = rhs.classList[i];    
+    }
+    return *this; //this will return the object in this function
+}
+
+void Student::inputClass()
+{
+    cout << "Name: \n ";
+    cin >> name;
+    cout << "Number of Classes: \n";
+    cin >> numClasses;
+    cin.ignore(2,'\n');
+
+    if(numClasses > 0)
+    {
+        classList = new string [numClasses];
+        for (int i = 0; i < numClasses; i++)
+        {
+            cout << "Class name " << (i+1) << " : ";
+            getline(cin, classList[i]);
+        }
+    }
+    cout << endl;
+}
+
+void Student::resetClass() 
+{
+    if(classList)
+    {
+        delete [] classList;
+        classList = NULL;
+        numClasses = 0;
+    }
+
+}
+
+void Student::printAll() const
+{
+    cout << "Name of the student: " << name << endl;
+    cout << "Class List:" << endl;
+    for(int i = 0; i < numClasses; i++)
+    {
+        cout << setw(2) << right << classList[i] << endl;
+    }
+}
+
+string Student::getName() const
+{
+    return name;
+}
+
+int Student::getNumclasses() const
+{
+    return numClasses;
+}
+
+/*
+line 9- this is the destructer that calls the resetClass function
+line 14- this will replace the content of the existing object  
+line 34- is for looks, this is used with getline so its not all piled into one line since we cant add the \n to a getline
+line 48- this is the function that will clear out the information once the function is done running.
+*/
+
--- Final/Q3/Student.hpp
+++ Final/Q3/Student.hpp
@@ -1,27 +1,31 @@
 #ifndef STUDENT_H
 #define STUDENT_H
+#include <iostream>
+#include <cstdlib>
 #include <string>
 using namespace std;
 
 class Student
 {
-    private:
-    string name;
-    int num_class;
-    string *class_list;
+    private: 
+        string name;
+        int numClasses;
+        string *classList;
 
-    public:
-    Student() : name(), num_class(0), class_list(NULL) {};
-    Student(string n, int num);
-    Student(Student &rhs); //copy const
-    ~Student();
-    Student &operator=(Student &rhs);
-    void inputClass(string n, int num);
-    void resetClass();
-    void printAll() const;
-    string getName() const;
-    int getNumclasses() const;
+    public: 
+        Student() : name(), numClasses(0), classList(NULL) {};
+        Student(string n, int num);
+        Student(const Student& rhs); //copy const
+        ~Student();
+        Student& operator=(const Student& rhs);
+        void inputClass();
+        void resetClass();
+        void printAll() const;
+        string getName() const;
+        int getNumclasses() const;
 
 };
-
 #endif
+/* I ended up removing the parameters in line 21. I didnt use them in the program and it 
+was coausing conflicts with my student.cpp. Since the program didnt have alot of information
+I didn see the need to create anyother variables. */
