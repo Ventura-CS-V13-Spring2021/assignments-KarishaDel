1b50ae440e0a7aa2f101d84948aa2f24f6764116
KarishaDel <karsishadelrosario@gmail.com>
2021-05-13 14:02:20 -0700

    1

--- Final/Q2.cpp
+++ Final/Q2.cpp
@@ -3,6 +3,7 @@ Write a program that reads in an array of type int. You may assume that there ar
 */
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 
 
 using namespace std;
@@ -10,5 +11,16 @@ using namespace std;
 int main()
 {
     int a[] = {-10, 10, 1, 2, 5, -13, 12, 15, -6};
+    int length= sizeof(a)/sizeof(a[0]); //cant use const becasue the size can change
+    int count;
 
+
+    sort(a, a + length, greater<int>()); //sort with algotithm
+    for(int i = 0; i < length; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    for(int)
+        
 }
