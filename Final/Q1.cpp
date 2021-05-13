ea90f54783892befe87fe3aa9467e5b66bfa10cd
KarishaDel <karsishadelrosario@gmail.com>
2021-05-13 12:25:59 -0700

    1

--- Final/Q1.cpp
+++ Final/Q1.cpp
@@ -1,10 +1,16 @@
+/* 
+You are given an array of integers that has the N positive integer elements with increasing sorted order. 
+Find the element that has the greatest number of divisible elements(the number of factors in the array). For example,
+*/
+
 #include <iostream>
 #include <iomanip>
 
-
 using namespace std;
+
+int getNumDiv(int num, int a[]);
 
 int main()
 {
-    
+
 }
